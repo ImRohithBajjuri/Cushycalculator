9f11b34f74ebc0a962fdab7304743464f2777a32
ImRohithBajjuri <rohithbajjuri520@gmail.com>
2018-12-29 19:33:50 +0530

    small ones

--- app/src/main/java/com/rohith/cushycalculator/MainActivity.java
+++ app/src/main/java/com/rohith/cushycalculator/MainActivity.java
@@ -3,6 +3,7 @@ package com.rohith.cushycalculator;
 import android.animation.LayoutTransition;
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.icu.math.BigDecimal;
 import android.support.constraint.ConstraintLayout;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +11,9 @@ import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
@@ -19,6 +23,7 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.view.animation.ScaleAnimation;
 import android.view.animation.TranslateAnimation;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
@@ -27,21 +32,30 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
+
+import com.udojava.evalex.Expression;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class MainActivity extends AppCompatActivity {
 
 
     TextView num1,num2,num3,num4,num5,num6,num7,num8,num9,num0,dot;
-    ImageView deleteTyped,addvalue,removevalue,dividevalue,multiplyvalue;
+    ImageView deleteTyped,addvalue,subtractvalue,dividevalue,multiplyvalue;
     CardView inputequatecard,inputoperatorscard,inputnumberscard;
     LinearLayout inputoperatorsparent,inputnumberslayout;
     ImageView opensientific,closescientific;
     RelativeLayout scientificinputlayout;
+    EditText displaycalculation;
+    TextView showequatedvalue;
 
+    String calculationtext;
+    String enteredvalue="";
+    int currentvalue;
 
 
     @Override
@@ -69,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
 
         //Input operators.
         addvalue=findViewById(R.id.addvalue);
-        removevalue=findViewById(R.id.removevalue);
+        subtractvalue=findViewById(R.id.subtractvalue);
         dividevalue=findViewById(R.id.dividevalue);
         multiplyvalue=findViewById(R.id.multiplyvalue);
         inputoperatorsparent=findViewById(R.id.inputoperatorsparent);
@@ -101,12 +115,62 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+        //Calculation display layout and its child views.
+        displaycalculation=findViewById(R.id.edittext);
+        showequatedvalue=findViewById(R.id.showequatedvalue);
+
+
+
+        displaycalculation.setShowSoftInputOnFocus(false);
+
+
+        displaycalculation.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                displaycalculation.setSelection(displaycalculation.getText().length());
+
+
+
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+
+
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
         //Input nums click listeners.
         num0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 clickAnim(num0);
+                enteredvalue=enteredvalue+"0";
+                currentvalue=0;
+
+                calculationtext=displaycalculation.getText().toString()+"0";
+
+
+                displaycalculation.setText(calculationtext);
             }
         });
 
@@ -116,6 +180,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 clickAnim(num1);
+
+                enteredvalue=enteredvalue+"1";
+                currentvalue=1;
+
+                calculationtext=displaycalculation.getText().toString()+"1";
+                displaycalculation.setText(calculationtext);
+
             }
         });
 
@@ -125,6 +196,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 clickAnim(num2);
+                enteredvalue=enteredvalue+"2";
+                currentvalue=2;
+
+                calculationtext=displaycalculation.getText().toString()+"2";
+                displaycalculation.setText(calculationtext);
             }
         });
 
@@ -134,6 +210,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 clickAnim(num3);
+
+                enteredvalue=enteredvalue+"3";
+                currentvalue=3;
+
+                calculationtext=displaycalculation.getText().toString()+"3";
+                displaycalculation.setText(calculationtext);
             }
         });
 
@@ -143,6 +225,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 clickAnim(num4);
+
+                enteredvalue=enteredvalue+"4";
+                currentvalue=4;
+
+                calculationtext=displaycalculation.getText().toString()+"4";
+                displaycalculation.setText(calculationtext);
             }
         });
 
@@ -152,6 +240,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 clickAnim(num5);
+
+                enteredvalue=enteredvalue+"5";
+                currentvalue=5;
+
+                calculationtext=displaycalculation.getText().toString()+"5";
+                displaycalculation.setText(calculationtext);
             }
         });
 
@@ -161,6 +255,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 clickAnim(num6);
+
+                enteredvalue=enteredvalue+"6";
+                currentvalue=6;
+
+                calculationtext=displaycalculation.getText().toString()+"6";
+                displaycalculation.setText(calculationtext);
             }
         });
 
@@ -170,6 +270,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 clickAnim(num7);
+
+                enteredvalue=enteredvalue+"7";
+                currentvalue=7;
+
+                calculationtext=displaycalculation.getText().toString()+"7";
+                displaycalculation.setText(calculationtext);
             }
         });
 
@@ -179,6 +285,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 clickAnim(num8);
+                enteredvalue=enteredvalue+"8";
+                currentvalue=8;
+
+                calculationtext=displaycalculation.getText().toString()+"8";
+                displaycalculation.setText(calculationtext);
             }
         });
 
@@ -188,6 +299,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 clickAnim(num9);
+                enteredvalue=enteredvalue+"9";
+                currentvalue=9;
+
+                calculationtext=displaycalculation.getText().toString()+"9";
+                displaycalculation.setText(calculationtext);
             }
         });
 
@@ -196,6 +312,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 clickAnim(dot);
+                enteredvalue=enteredvalue+".";
+                calculationtext=displaycalculation.getText().toString()+".";
+                displaycalculation.setText(calculationtext);
             }
         });
 
@@ -215,14 +334,44 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 clickAnim(addvalue);
+
+
+                calculationtext=displaycalculation.getText().toString()+"+";
+
+                displaycalculation.setText(calculationtext);
+
+
+
+
+
+
+
+
+
             }
         });
 
 
-        removevalue.setOnClickListener(new View.OnClickListener() {
+        subtractvalue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                clickAnim(removevalue);
+                clickAnim(subtractvalue);
+
+                calculationtext=displaycalculation.getText().toString()+"-";
+
+
+
+
+                displaycalculation.setText(calculationtext);
+
+
+
+
+
+
+
+
+
             }
         });
 
@@ -231,6 +380,16 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 clickAnim(dividevalue);
+
+                calculationtext=displaycalculation.getText().toString()+"÷";
+
+                displaycalculation.setText(calculationtext);
+
+
+
+
+
+
             }
         });
 
@@ -239,6 +398,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 clickAnim(multiplyvalue);
+
+                calculationtext=displaycalculation.getText().toString()+"×";
+
+
+
+                displaycalculation.setText(calculationtext);
+
+
+
+
+
             }
         });
 
@@ -261,6 +431,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 clickAnim(inputequatecard);
+
+
+
+
+                if (!TextUtils.isEmpty(calculationtext)){
+                    Expression expression=new Expression(calculationtext.replace("×","*").replace("÷","/"));
+                    java.math.BigDecimal equatevalue=expression.eval();
+
+                    showequatedvalue.setText(String.valueOf(equatevalue));
+                }
+
+
+
+
             }
         });
 
@@ -295,6 +479,17 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+        deleteTyped.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String textString = displaycalculation.getText().toString();
+                if( textString.length() > 0 ) {
+                    displaycalculation.setText(textString.substring(0, textString.length() - 1 ));
+                    displaycalculation.setSelection(displaycalculation.getText().length());
+                    calculationtext=displaycalculation.getText().toString();
+                }
+            }
+        });
 
 
 
@@ -319,6 +514,52 @@ public class MainActivity extends AppCompatActivity {
         view.startAnimation(ClickAnimation);
 
     }
+
+
+
+
+    //Methods for basic input operators following BODMAS rules.
+
+    public void division(int inputValue){
+       int totalvalue =Integer.valueOf(calculationtext)/inputValue;
+
+       calculationtext=String.valueOf(totalvalue);
+
+        showequatedvalue.setText(calculationtext);
+    }
+
+    public void multiplication(int inputValue){
+       int totalvalue=Integer.valueOf(calculationtext)*inputValue;
+
+        calculationtext=String.valueOf(totalvalue);
+
+        showequatedvalue.setText(calculationtext);
+    }
+
+    public void addition(int inputValue){
+       int totalvalue=Integer.valueOf(calculationtext)+inputValue;
+
+        calculationtext=String.valueOf(totalvalue);
+
+        showequatedvalue.setText(calculationtext);
+    }
+
+    public void subtraction(int inputValue){
+       int totalvalue=Integer.valueOf(calculationtext)-inputValue;
+
+        calculationtext=String.valueOf(totalvalue);
+
+        showequatedvalue.setText(calculationtext);
+    }
+
+
+
+    public void equate(){
+
+    }
+
+
+
 
 
 
